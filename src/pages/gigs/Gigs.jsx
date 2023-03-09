780b6fc5bf2577aefe6c2d48d5585f5e63e5a695
GoNiazi <komailniazi@gmail.com>
2023-03-09 15:33:35 +0500

    frontend completed

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Gigs.scss";
+import { gigs } from "../../data.js";
+import GigCard from "./../../components/gigCard/GigCard";
+
 const Gigs = () => {
-  return <div>Gigs</div>;
+  const [open, setopen] = useState(false);
+  const [sort, setsort] = useState("sales");
+  return (
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">fiverr > Graphics & Design </span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with Liverr's AI artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">Sort by</span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}
+            </span>
+            <img
+              src="/imgs/down.png"
+              alt="down"
+              onClick={() => setopen(!open)}
+            />
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => setsort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => setsort("sales")}>Best Selling</span>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="cards">
+          {gigs.map((gig) => {
+            return <GigCard item={gig} key={gig.id} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gigs;
